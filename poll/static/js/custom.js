15bf6bf6b0f00053fc67ca7cdd5212e4d5e60d72
Sling_J <nurel32@gmail.com>
2019-02-20 22:58:56 +0600

    Slider effects was added

--- poll/static/js/custom.js
+++ poll/static/js/custom.js
@@ -1,4 +1,8 @@
 const onSubmitForm = document.querySelectorAll('.onSubmitForm');
+const wrapBox = document.querySelector('.wrap-box');
+let currentWrap = wrapBox.firstElementChild;
+
+currentWrap.classList.add('active');
 
 onSubmitForm.forEach(item => {
    item.addEventListener('submit', sendForm)
@@ -19,4 +23,18 @@ function sendForm(e) {
          console.log('Vote counted');
       }
    });
+   
+   const activeWrap = currentWrap.nextElementSibling;
+
+   currentWrap.classList.remove('active');
+   activeWrap.classList.add('active');
+
+   currentWrap = activeWrap;
+
+   if (activeWrap.nextElementSibling === null) {
+      activeWrap.classList.remove('active');
+      wrapBox.textContent = 'Спасибо за голосование!';
+   };
+
+   if (!activeWrap) return;
 };
